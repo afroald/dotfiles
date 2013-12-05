526b51a6de97cf8f004055314ac1bf858ed94f5d
Roald Hacquebord <afroald@gmail.com>
2013-12-05 17:16:48 +0100

    Disable Dashboard

--- set-defaults.sh
+++ set-defaults.sh
@@ -28,3 +28,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Who even uses the Dashboard?
+defaults write com.apple.dashboard mcx-disabled -boolean true
