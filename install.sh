bf06e760f63f7d98d441f69578119ba891a167cd
Roald Hacquebord <roald@decorrespondent.nl>
2015-06-25 09:25:18 +0200

    I don't use Pow anymore

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ ln -s .dotfiles/.gitconfig .gitconfig
 ln -s .dotfiles/.global_ignore .global_ignore
 ln -s .dotfiles/.zshenv .zshenv
 ln -s .dotfiles/.zshrc .zshrc
-ln -s .dotfiles/.powconfig .powconfig
