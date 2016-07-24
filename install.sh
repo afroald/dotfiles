d386787f21f8fad8dd2ef4cfa95fe36e0c2588ee
Roald Hacquebord <roald@hacquebordbuckles.nl>
2016-07-24 19:24:14 +0200

    Add nano config

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 cd ~
 ln -s .dotfiles/.gitconfig .gitconfig
 ln -s .dotfiles/.global_ignore .global_ignore
+ln -s .dotfiles/.nanorc .nanorc
 ln -s .dotfiles/.zshenv .zshenv
 ln -s .dotfiles/.zshrc .zshrc
