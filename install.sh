ca5f4b17de7b91e41f3e324b1f6ec1e40fd24e80
Roald Hacquebord <roald@hacquebordbuckles.nl>
2016-12-20 11:28:52 +0100

    Add Hyper config

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-cd ~
+cd "$HOME"
 ln -s .dotfiles/.gitconfig .gitconfig
 ln -s .dotfiles/.global_ignore .global_ignore
+ln -s .dotfiles/.hyper.js .hyper.js
 ln -s .dotfiles/.nanorc .nanorc
 ln -s .dotfiles/.zshenv .zshenv
 ln -s .dotfiles/.zshrc .zshrc
+
