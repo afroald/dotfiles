fb6dd54ee909457d17b0476967e20290f4399136
Roald Hacquebord <roald@zielma.kr>
2020-08-20 10:56:45 +0200

    Remove hyper config

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ cd "$HOME"
 
 ln -sf .dotfiles/.gitconfig .gitconfig
 ln -sf .dotfiles/.global_ignore .global_ignore
-ln -sf .dotfiles/.hyper.js .hyper.js
 ln -sf .dotfiles/.nanorc .nanorc
 ln -sf .dotfiles/.zshenv .zshenv
 ln -sf .dotfiles/.zshrc .zshrc
