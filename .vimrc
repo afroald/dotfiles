da718248f6764798371a9335fe0d228ed5770b47
Roald Hacquebord <roald@zielma.kr>
2022-12-23 16:18:22 +0100

    Add vimrc

---
+++ .vimrc
@@ -0,0 +1,3 @@
+syntax on
+set nu rnu
+
--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ ln -sf .dotfiles/.global_ignore .global_ignore
 ln -sf .dotfiles/.nanorc .nanorc
 ln -sf .dotfiles/.zshenv .zshenv
 ln -sf .dotfiles/.zshrc .zshrc
+ln -sf .dotfiles/.vimrc .vimrc
+
