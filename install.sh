bcb2f6135e79375508e74f2705a84c4f1415f96c
Roald Hacquebord <roald@zielma.kr>
2023-05-10 11:24:38 +0200

    Nano is for noobs

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ cd "$HOME"
 
 ln -sf .dotfiles/.gitconfig .gitconfig
 ln -sf .dotfiles/.global_ignore .global_ignore
-ln -sf .dotfiles/.nanorc .nanorc
 ln -sf .dotfiles/.zshenv .zshenv
 ln -sf .dotfiles/.zshrc .zshrc
 ln -sf .dotfiles/.vimrc .vimrc
