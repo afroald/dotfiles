923fb39a0423e5256c1e444a0432114a791038c4
Roald Hacquebord <roald@zielma.kr>
2023-05-10 11:24:47 +0200

    Fix bug

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ ln -sf .dotfiles/.global_ignore .global_ignore
 ln -sf .dotfiles/.zshenv .zshenv
 ln -sf .dotfiles/.zshrc .zshrc
 ln -sf .dotfiles/.vimrc .vimrc
-ln -sf ../.dotfiles/nvim .config/nvim
+ln -sf .dotfiles/nvim .config/nvim
 
