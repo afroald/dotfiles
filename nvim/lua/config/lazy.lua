f5f9bbbec6c3373b6599ebbc3a16ec064cb5ccad
Roald Hacquebord <roald@zielma.kr>
2023-08-16 09:41:01 +0200

    Load yanky plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     -- import/override with your plugins
     { import = "plugins" },
   },
