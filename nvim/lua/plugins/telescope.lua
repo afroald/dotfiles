e95553a59a40efc96f597d143318466f97fb3496
Roald Hacquebord <roald@zielma.kr>
2023-09-26 22:53:10 +0200

    Load fzf plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ local util = require("lazyvim.util")
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
     keys = {
       { "<leader><space>", util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
     },
