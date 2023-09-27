151d3bf3cffe86eeb0bb3e98f806677634fc60df
Roald Hacquebord <roald@zielma.kr>
2023-09-27 10:27:48 +0200

    Remove custom organize imports command
    The typescript integration already provides this. See this page for more information:
    https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/typescript.lua#L23

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,5 @@
 local util = require("lspconfig.util")
 
-local function organise_imports()
-  vim.lsp.buf.execute_command({
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
-  })
-end
-
 return {
   {
     "neovim/nvim-lspconfig",
@@ -24,18 +16,6 @@ return {
         intelephense = {
           init_options = {
             licenceKey = "/Users/roald/.dotfiles/intelephense-license.txt",
-          },
-        },
-        tsserver = {
-          commands = {
-            OrganiseImports = {
-              organise_imports,
-              description = "Organise Imports",
-            },
-            OrganizeImports = {
-              organise_imports,
-              description = "Organise Imports",
-            },
           },
         },
       },
