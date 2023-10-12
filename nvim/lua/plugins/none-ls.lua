cd089d541e77da40ee9ea3647bd06ce27f7dba7c
Roald Hacquebord <roald@zielma.kr>
2023-10-12 23:22:32 +0200

    Replace null-ls with none-ls

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.diagnostics.phpstan)
+    end,
+  },
+}
