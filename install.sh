4c4319d690f5e54835a5bcfb44ac34f7182300ef
Roald Hacquebord <roald@zielma.kr>
2024-01-29 14:13:38 +0100

    Add config for starship prompt

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e
 
 cd "$HOME"
 
-ln -sf .dotfiles/.gitconfig .gitconfig
-ln -sf .dotfiles/.global_ignore .global_ignore
-ln -sf .dotfiles/.zshenv .zshenv
-ln -sf .dotfiles/.zshrc .zshrc
-ln -sf .dotfiles/.vimrc .vimrc
-ln -sf ~/.dotfiles/nvim .config/nvim
-
+ln -sf .dotfiles/.gitconfig ~/.gitconfig
+ln -sf .dotfiles/.global_ignore ~/.global_ignore
+ln -sf .dotfiles/.vimrc ~/.vimrc
+ln -sf .dotfiles/.zshenv ~/.zshenv
+ln -sf .dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/starship.toml ~/.config/starship.toml
