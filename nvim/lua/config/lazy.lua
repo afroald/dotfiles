e7b489f9e63b11ad55ab33228b40203d16526bd9
Roald Hacquebord <roald@zielma.kr>
2024-09-09 16:35:31 +0200

    Enable neovim prettier

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
