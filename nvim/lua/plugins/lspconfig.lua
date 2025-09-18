ae1ea20e288f7877b0f62c4f0487c288ed1515bd
Roald Hacquebord <roald@zielma.kr>
2025-09-18 13:38:28 +0200

    Fix lsp config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,15 +7,6 @@ return {
       servers = {
         vtsls = {
           init_options = { hostInfo = "neovim" },
-          root_dir = util.find_git_ancestor,
-        },
-        eslint = {
-          root_dir = util.find_git_ancestor,
-          settings = {
-            workingDirectory = {
-              mode = "auto",
-            },
-          },
         },
       },
     },
