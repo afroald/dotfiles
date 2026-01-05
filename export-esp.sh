d323c0e516fe632dd73c801d102661bb985d10f3
Roald Hacquebord <roald@zielma.kr>
2026-01-05 09:17:02 +0100

    Update esp toolchain

--- export-esp.sh
+++ export-esp.sh
@@ -1,2 +1,2 @@
-export PATH="/Users/roald/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/Users/roald/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-19.1.2_20250225/esp-clang/lib"
+export PATH="/Users/roald/.rustup/toolchains/esp/xtensa-esp-elf/esp-15.2.0_20250920/xtensa-esp-elf/bin:$PATH"
+export LIBCLANG_PATH="/Users/roald/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-20.1.1_20250829/esp-clang/lib"
